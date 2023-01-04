df17fdd64aa7fa67d92fa177a5a8a4478e7e902a
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-04 19:31:11 +0300

    created Input in Chat and styles for it

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Img from '../assets/img/img.png'
+import Attach from '../assets/img/attach.png'
 
 const Input = () => {
-  return <div className="input">Input</div>
+  return (
+    <div className="input">
+      <input type="text" placeholder="Написать сообщение..." />
+      <div className="send">
+        <img src={Attach} alt="add attach png" />
+        <input type="file" style={{ display: 'none' }} id="file" />
+        <label htmlFor="file">
+          <img src={Img} alt="add pic png" />
+        </label>
+        <button>Отправить</button>
+      </div>
+    </div>
+  )
 }
 
 export default Input
