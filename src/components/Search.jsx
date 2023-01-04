10c09a81bf3e0e3a8280dd66a8b3d9aa7c3d0785
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-04 18:48:10 +0300

    created Search comp and styles for it

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Face from '../assets/img/1.jpg'
 
 const Search = () => {
-  return <div>Search</div>
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Найти пользователя..." />
+      </div>
+      <div className="userChat">
+        <img src={Face} alt="user photo" />
+        <div className="userChatInfo">
+          <span>Ruslan</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Search
