f388647a3dd9e7742ef78ea3b5895602f2deeb23
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-05 22:31:04 +0300

    added register with firebase

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AddAvatar from '../assets/img/addAvatar.png'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { auth, storage } from '../firebase'
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 
 function Register() {
+  const [err, setErr] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const displayName = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+    const file = e.target[3].files[0]
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+
+      const storageRef = ref(storage, displayName)
+
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      uploadTask.on(
+        (error) => {
+          setErr(true)
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName: displayName,
+              photoURL: downloadURL,
+            })
+          })
+        }
+      )
+    } catch (err) {
+      setErr(true)
+    }
+  }
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">TALK Чат</span>
         <span className="title">Регистрация</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Имя"></input>
           <input type="email" placeholder="Email"></input>
           <input type="password" placeholder="Пароль"></input>
@@ -17,6 +55,7 @@ function Register() {
             <span>Выбрать аватарку</span>
           </label>
           <button>Зарегистрироваться</button>
+          {err && <span>Что-то пошло не так...</span>}
           <p>Уже есть аккаунт? Войти</p>
         </form>
       </div>
