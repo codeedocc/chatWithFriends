cb128865dd7723ad51a4c59448c5dc639e31726e
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-05 23:48:30 +0300

    now we can add photo while register

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_SECRET_KEY,
-  authDomain: 'advanced-chat-app-20c0f.firebaseapp.com',
-  projectId: 'advanced-chat-app-20c0f',
-  storageBucket: 'advanced-chat-app-20c0f.appspot.com',
-  messagingSenderId: '923649806682',
-  appId: '1:923649806682:web:d18127acdc7cf8ef676363',
+  authDomain: 'advanced-chat-app-4c67a.firebaseapp.com',
+  projectId: 'advanced-chat-app-4c67a',
+  storageBucket: 'advanced-chat-app-4c67a.appspot.com',
+  messagingSenderId: '604278974782',
+  appId: '1:604278974782:web:0eeca72c825271b394e46f',
 }
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth()
 export const storage = getStorage()
+export const db = getFirestore()
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,40 +1,48 @@
 import React, { useState } from 'react'
 import AddAvatar from '../assets/img/addAvatar.png'
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
-import { auth, storage } from '../firebase'
+import { auth, db, storage } from '../firebase'
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { doc, setDoc } from 'firebase/firestore'
 
 function Register() {
   const [err, setErr] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-
     const displayName = e.target[0].value
     const email = e.target[1].value
     const password = e.target[2].value
     const file = e.target[3].files[0]
 
     try {
+      //Create user
       const res = await createUserWithEmailAndPassword(auth, email, password)
 
+      //Create a unique image name
       const storageRef = ref(storage, displayName)
 
-      const uploadTask = uploadBytesResumable(storageRef, file)
-
-      uploadTask.on(
-        (error) => {
-          setErr(true)
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
             await updateProfile(res.user, {
-              displayName: displayName,
+              displayName,
               photoURL: downloadURL,
             })
-          })
-        }
-      )
+            //create user on firestore
+            await setDoc(doc(db, 'users', res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            })
+          } catch (err) {
+            console.log(err)
+            setErr(true)
+          }
+        })
+      })
     } catch (err) {
       setErr(true)
     }
