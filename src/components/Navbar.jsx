cd4f7f56602e81505b509a49064ba541c073906c
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-06 19:26:20 +0300

    created user search logic in Search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import { signOut } from 'firebase/auth'
 import React from 'react'
+import { useContext } from 'react'
 import Face from '../assets/img/1.jpg'
+import { AuthContext } from '../context/AuthContext'
 import { auth } from '../firebase'
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext)
+
   return (
     <div className="navbar">
       <span className="logo">TALK Чат</span>
       <div className="user">
-        <img src={Face} />
-        <span>Руслан</span>
+        <img src={currentUser.photoURL} />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>Выйти</button>
       </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Face from '../assets/img/1.jpg'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { db } from '../firebase'
 
 const Search = () => {
+  const [userName, setUserName] = useState('')
+  const [user, setUser] = useState(null)
+  const [err, setErr] = useState(false)
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, 'users'),
+      where('displayName', '==', userName)
+    )
+
+    try {
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data())
+      })
+    } catch (e) {
+      setErr(true)
+    }
+  }
+
+  const handlePress = (e) => {
+    e.code === 'Enter' && handleSearch()
+  }
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Найти пользователя..." />
+        <input
+          type="text"
+          placeholder="Найти пользователя..."
+          onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handlePress}
+        />
       </div>
-      <div className="userChat">
-        <img src={Face} alt="user" />
-        <div className="userChatInfo">
-          <span>Ruslan</span>
+      {err && <span>Что-то пошло не так...</span>}
+      {user && (
+        <div className="userChat">
+          <img src={user.photoURL} alt="user" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
